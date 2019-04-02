2bcccf824262bd84f2fbbeebda1563fd3effda84
Raymond Hernandez <rhernandez028@west-mec.org>
2019-04-01 18:21:15 -0700

    Appended elements and styles
    create several commands to add html element to the index using JavaScript

--- bc_switch.js
+++ bc_switch.js
@@ -17,3 +17,43 @@
    
 */
 
+window.addEventListener("load", setupStyles);
+
+function setupStyles() {
+    //creat a link element for the page view styles
+    var pageStyle = document.createElement("link");
+    pageStyle.setAttribute("href", "bc_page.css");
+    pageStyle.setAttribute("rel", "stylesheet");
+    pageStyle.setAttribute("disabled", "disabled");
+
+    //append the link to the document head
+    document.head.appendChild(pageStyle);
+    pageStyle.disabled = true;
+
+    //insert buttons for the style switcher
+    var buttonDIV = document.createElement("div");
+    buttonDIV.setAttribute("id", "styleButtons");
+
+    var webButton = document.createElement("input");
+    webButton.setAttribute("type", "button");
+    webButton.setAttribute("value", "Web View");
+
+    var pageButton = document.createElement("input");
+    pageButton.setAttribute("type", "button");
+    pageButton.setAttribute("value", "Page View");
+
+    buttonDIV.appendChild(webButton);
+    buttonDIV.appendChild(pageButton);
+
+    document.body.insertBefore(buttonDIV, document.body.firstChild);
+
+    //append an embedded style sheet to the document head
+    var buttonStyles = document.createElement("style");
+    documnet.head.appendChild(buttonStyles);
+
+    //add style rules to the embedded style sheet
+    document.styleSheets[document.styleSheets.length - 1].insertRule(
+        "div#styleButtons { \
+            position: fixed; \
+        }", 0);
+}
